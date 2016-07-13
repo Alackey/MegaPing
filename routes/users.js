b7d2a8c22e9eb9547f6b43c515b07eb1bb45155d
Anthony Lackey <alackey96@gmail.com>
2016-07-13 01:36:59 -0700

    Get posts from megalinks in json and return them

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  let posts = function(callback) {
+    https.get('https://www.reddit.com/r/megalinks/new.json?limit=2', function(megaPosts) {
+
+      megaPosts.setEncoding('utf8');
+
+      var jsonRes = '';
+      megaPosts.on('data', (d) => {
+        jsonRes += d;
+      });
+
+      megaPosts.on('end', function() {
+        var parsed = JSON.parse(JSON.stringify(jsonRes));
+        callback(parsed);
+      });
+    });
+  };
+
+  posts(function(allPosts) {
+    res.send(allPosts);
+  });
+
 });
 
 module.exports = router;
