62838064a699ab9c95e894dcc355200dc4320ace
Anthony Lackey <alackey96@gmail.com>
2016-07-25 23:41:34 -0700

    React: create NewPingBox, NewPingForm components

--- public/javascripts/newPing.js
+++ public/javascripts/newPing.js
@@ -1,4 +1,38 @@
+var NewPingBox = React.createClass({
+  render: function() {
+    return (
+      <div className='newPingBox'>
+        <p>New Ping</p>
+        <NewPingForm />
+      </div>
+    );
+  }
+});
+
+var NewPingForm = React.createClass({
+  render: function() {
+    return (
+      <p>New Ping form</p>
+      // <form className="commentForm" onSubmit={this.handleSubmit}>
+      //   <input
+      //     type="text"
+      //     placeholder="Your name"
+      //     value={this.state.author}
+      //     onChange={this.handleAuthorChange}
+      //   />
+      //   <input
+      //     type="text"
+      //     placeholder="Say something..."
+      //     value={this.state.text}
+      //     onChange={this.handleTextChange}
+      //   />
+      //   <input type="submit" value="Post" />
+      // </form>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>New Ping!!</h1>,
+  <NewPingBox />,
   document.getElementById('newPingContainer')
 );
